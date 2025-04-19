8bfc1f20e84c88820a387d4139f0f1b194d0013d
BMInBC <fawazadekanmbi19@gmail.com>
2025-04-19 23:04:40 +0100

    Add files via upload

---
+++ assigbnment_day11.py
@@ -0,0 +1,23 @@
+#exaple of a tuple
+
+my_tuple = (1, 2, 3)
+print('example of a tuple')
+print(my_tuple[0])  
+print(my_tuple[1])  
+print(my_tuple[2])  
+# Output: 1
+
+print('Application of range')
+for i in range(5):
+    print(i)
+
+
+#example of a list
+print('Application of a list')
+my_list = [1, 2, 3]
+my_list[0] = 10
+my_list.append(4)
+print(my_list)  
+
+
+
